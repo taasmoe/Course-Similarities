af05cecfff9f202c54cf2ab151e7754947423f81
taasmoe <tobias.aasmoe@gmail.com>
2017-02-22 15:57:41 +0100

    Ready for similartiy queries

--- similarity_queries.py
+++ similarity_queries.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import pickle
 
 from pprint import pprint
 from gensim import corpora, models, similarities
@@ -9,12 +10,27 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 dictionary = None
 corpus = None
 
-
 if os.path.exists("course_dictionary.dict"):
-    dictionary = corpora.Dictionary.load("dictionary.dict")
+    dictionary = corpora.Dictionary.load("course_dictionary.dict")
     corpus = corpora.MmCorpus("corpus.mm")
 else:
     print("Error")
 
+with open("course_info.txt", "rb") as f:
+    course_info = pickle.load(f)
+
 lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
 
+print(course_info[0])
+test_doc = course_info[0]
+vec_bow = dictionary.doc2bow(test_doc)
+vec_lsi = lsi[vec_bow]
+
+index = similarities.MatrixSimilarity(lsi[corpus])
+index.save("sim-index.index")
+
+sims = index[vec_lsi]
+
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+print(sims)
